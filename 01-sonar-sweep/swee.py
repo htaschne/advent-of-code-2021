0894ea288af6d36d089c730bb9a03f734f0e4102
Pedro Schneider <pedroschneider@outlook.com>
2021-12-02 00:52:18 -0300

    refactor

--- 01-sonar-sweep/swee.py
+++ 01-sonar-sweep/swee.py
@@ -5,5 +5,5 @@ import sys
 
 dps = [int(l.rstrip()) for l in open(sys.argv[1]).readlines()]
 
-print(sum([1 for i in range(len(dps) - 1) if dps[i] < dps[i + 1]]))
-print(sum([1 for i in range(len(dps) - 3) if dps[i] + dps[i+1] + dps[i+2] < dps[i+1]+ dps[i+2] + dps[i+3]]))
+print(sum([dps[i] < dps[i + 1] for i in range(len(dps) - 1)]))
+print(sum([dps[i] + dps[i+1] + dps[i+2] < dps[i+1]+ dps[i+2] + dps[i+3] for i in range(len(dps) - 3)]))
