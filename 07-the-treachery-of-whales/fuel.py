842c12b605cc6a00cb6466520010138ff932a1fb
Pedro Schneider <pedroschneider@outlook.com>
2021-12-07 08:14:29 -0300

    crabs aligned

--- 07-the-treachery-of-whales/fuel.py
+++ 07-the-treachery-of-whales/fuel.py
@@ -8,13 +8,26 @@ def fuel(pos, lst):
     acc += abs(pos - h)
   return acc
 
+def inc_fuel(pos, lst):
+  acc = 0
+  for h in lst:
+    move_cost = 1
+    for i in range(abs(pos - h)):
+      acc += move_cost
+      move_cost += 1
+  return acc
+
 def main():
   h = list(map(int, open(sys.argv[1]).readline().split(",")))
-  print(h)
 
-  best = 10000000
+  best = 10000000000000
   for i in range(max(h)):
     best = min(fuel(i, h), best)
+  print(best)
+
+  best = 10000000000000
+  for i in range(max(h)):
+    best = min(inc_fuel(i, h), best)
   print(best)
 
 
