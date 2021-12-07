1e875c21393a12e76c8c43cf24ec3faa4f89cf50
Pedro Schneider <pedroschneider@outlook.com>
2021-12-07 08:51:03 -0300

    gotta go fast
    add types
    
    remove for loops

--- 07-the-treachery-of-whales/fuel.py
+++ 07-the-treachery-of-whales/fuel.py
@@ -1,34 +1,28 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 import sys
 
-def fuel(pos, lst):
+from functools import lru_cache
+
+def fuel(pos: int, lst: list[int]) -> int:
   acc = 0
   for h in lst:
     acc += abs(pos - h)
   return acc
 
-def inc_fuel(pos, lst):
+@lru_cache
+def s(x: int) -> int:
+  return x * (1 + x) // 2
+
+def inc_fuel(pos: int, lst: list[int]) -> int:
   acc = 0
   for h in lst:
-    move_cost = 1
-    for i in range(abs(pos - h)):
-      acc += move_cost
-      move_cost += 1
+    acc += s(abs(pos - h))
   return acc
 
 def main():
   h = list(map(int, open(sys.argv[1]).readline().split(",")))
-
-  best = 10000000000000
-  for i in range(max(h)):
-    best = min(fuel(i, h), best)
-  print(best)
-
-  best = 10000000000000
-  for i in range(max(h)):
-    best = min(inc_fuel(i, h), best)
-  print(best)
-
+  print(min(fuel(i, h) for i in range(max(h))))
+  print(min(inc_fuel(i, h) for i in range(max(h))))
 
 main()
