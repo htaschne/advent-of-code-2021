91960114297cb1aff12322ef6a6deb33534d4015
Pedro Schneider <pedroschneider@outlook.com>
2021-12-07 09:58:06 -0300

    smol

--- 07-the-treachery-of-whales/fuel.py
+++ 07-the-treachery-of-whales/fuel.py
@@ -5,20 +5,14 @@ import sys
 from functools import lru_cache
 
 def fuel(pos: int, lst: list[int]) -> int:
-  acc = 0
-  for h in lst:
-    acc += abs(pos - h)
-  return acc
+  return sum([abs(pos - h) for h in lst])
 
 @lru_cache
 def s(x: int) -> int:
   return x * (1 + x) // 2
 
 def inc_fuel(pos: int, lst: list[int]) -> int:
-  acc = 0
-  for h in lst:
-    acc += s(abs(pos - h))
-  return acc
+  return sum([s(abs(pos - h)) for h in lst])
 
 def main():
   h = list(map(int, open(sys.argv[1]).readline().split(",")))
