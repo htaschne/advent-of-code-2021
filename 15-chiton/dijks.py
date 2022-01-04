56b5eaa823027afeb77dc970b7c0c51c26722501
Pedro Schneider <pedroschneider@outlook.com>
2022-01-04 14:28:16 -0300

    add part 2
    add part 2

--- 15-chiton/dijks.py
+++ 15-chiton/dijks.py
@@ -5,8 +5,27 @@ import sys
 
 from dijkstar import Graph, find_path
 
-def main():
 
+def expand(edges, mr, mc):
+  new_edges = {}
+
+  for tile_row in range(5):
+    for tile_col in range(5):
+      for i in range(mr):
+        for j in range(mc):
+          new_risk = edges[(i, j)] + tile_row + tile_col
+
+          if new_risk > 9:
+            new_risk -= 9
+
+          assert new_risk <= 9
+
+          new_edges[(tile_row * mr + i, tile_col * mc + j)] = new_risk
+
+  return new_edges
+
+
+def main():
   edges = {}
   mr, mc = 0, 0
   for r, row in enumerate(open(sys.argv[1]).readlines()):
@@ -24,7 +43,22 @@ def main():
         w = edges[(row, col)]
         g.add_edge((r, c), (row, col), w)
 
-  ret = find_path(g, (0, 0), (mr, mc))
-  print(ret)
+  min_path = find_path(g, (0, 0), (mr, mc)).total_cost
+  print(min_path)
+
+  edges = expand(edges, mr + 1, mc + 1)
+
+  g = Graph()
+  for r, c in edges:
+    for dr, dc in ((1, 0), (-1, 0), (0, 1), (0, -1)):
+      row = r + dr
+      col = c + dc
+      if (row, col) in edges:
+        w = edges[(row, col)]
+        g.add_edge((r, c), (row, col), w)
+
+  min_path = find_path(g, (0, 0), ((mr * 5) + 4, (mc * 5) + 4)).total_cost
+  print(min_path)
+
 
 main()
